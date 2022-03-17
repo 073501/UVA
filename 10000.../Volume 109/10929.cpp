a0597b29b4182a67af3badbda5f7de7a92c14fa5
073501 <92313395+073501@users.noreply.github.com>
2022-03-17 19:17:57 +0800

    Create 10929.cpp

---
+++ 10000.../Volume 109/10929.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string input;
+    while (cin >> input) {
+        int odd = 0, even = 0;
+        if (input == "0") break;
+        for (int i = 0; i < input.length(); i++) {
+            if (i % 2 == 0) odd += input[i] - '0';
+            else even += input[i] - '0';
+        }
+        cout << input << " is";
+        if (abs(odd - even) % 11 != 0) cout << " not";
+        cout << " a mutiple of 11." << endl;
+    }
+    return 0;
+}
