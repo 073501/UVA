14d7084be5a8e27cdd8c6c76af173276ce9675af
073501 <92313395+073501@users.noreply.github.com>
2022-03-17 19:19:23 +0800

    Create 100.cpp

---
+++ 100...1999/Volume 1/100.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int solve(int n) {
+    int count = 1;
+    while (n != 1) {
+        if (n % 2 == 0) n /= 2;
+        else n = 3 * n + 1;
+        count++;
+    }
+    return count;
+}
+
+int main() {
+    int i, j;
+    while (cin >> i >> j) {
+        int ans = 0;
+        for (int k = i; k <= j; k++) {
+            ans = max(ans, solve(k));
+        }
+        cout << i << " " << j << " " << ans << endl;
+    }
+    return 0;
+}
