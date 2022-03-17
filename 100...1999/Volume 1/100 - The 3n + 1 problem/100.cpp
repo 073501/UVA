a0c6d391dcae0e0757de4bfcbf0379197b183998
073501 <92313395+073501@users.noreply.github.com>
2022-03-17 19:28:52 +0800

    Update 100.cpp

--- 100...1999/Volume 1/100 - The 3n + 1 problem/100.cpp
+++ 100...1999/Volume 1/100 - The 3n + 1 problem/100.cpp
@@ -15,11 +15,12 @@ int main() {
     int i, j;
     while (cin >> i >> j) {
         int ans = 0;
+        cout << i << " " << j << " ";
         if (i > j) swap(i, j);
         for (int k = i; k <= j; k++) {
             ans = max(ans, solve(k));
         }
-        cout << i << " " << j << " " << ans << endl;
+        cout << ans << endl;
     }
     return 0;
 }
