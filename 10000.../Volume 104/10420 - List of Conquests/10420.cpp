9ad61eb31ed7a8df232ee70257d3a0632a3b980c
073501 <92313395+073501@users.noreply.github.com>
2022-03-21 17:49:34 +0800

    Create 10420.cpp

---
+++ 10000.../Volume 104/10420 - List of Conquests/10420.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
+
+int main() {
+    int n;
+    string country;
+    map<string, int> m;
+    cin >> n;
+    while (n--) {
+        cin >> country;
+        m[country]++;
+        getline(cin, country);
+    }
+    for (auto it = m.begin(); it != m.end(); it++) {
+        cout << it->first << " " << it->second << endl;
+    }
+    return 0;
+}
