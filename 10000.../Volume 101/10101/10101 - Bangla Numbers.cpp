a99800de53032084161af7a38556bf3d98d9a44e
073501 <92313395+073501@users.noreply.github.com>
2022-03-21 17:10:20 +0800

    Create 10101 - Bangla Numbers.cpp

---
+++ 10000.../Volume 101/10101/10101 - Bangla Numbers.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void solve(long long n) {
+    if (n >= 10000000) {
+        solve(n / 10000000);
+        cout << " kuti";
+        n %= 10000000;
+    }
+    if (n >= 100000) {
+        solve(n / 100000);
+        cout << " lakh";
+        n %= 100000;
+    }
+    if (n >= 1000) {
+        solve(n / 1000);
+        cout << " hajar";
+        n %= 1000;
+    }
+    if (n >= 100) {
+        solve(n / 100);
+        cout << " shata";
+        n %= 100;
+    }
+    if (n) {
+        cout << " " << n;
+    }
+}
+
+int main() {
+    long long n;
+    int cases = 1;
+    while (cin >> n) {
+        cout << setw(4) << cases++ << ".";
+        if (n) solve(n);
+        else cout << " 0";
+        cout << endl;
+    }
+    return 0;
+}
